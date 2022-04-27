2cb551d832eaabb1017397239e2011cc55a0db69
Hooooon12 <theking461@snu.ac.kr>
2022-04-27 16:24:02 +0900

    .

---
+++ NonIso.sh
@@ -0,0 +1,19 @@
+#python python/SKFlat.py -a NonIso -e 2016a -i SingleMuon -n 50 --batchname auto &
+#python python/SKFlat.py -a NonIso -e 2016a -i DYJets -n 50 --batchname auto &
+#python python/SKFlat.py -a NonIso -e 2016a -i WJets_MG -n 50 --batchname auto &
+#python python/SKFlat.py -a NonIso -e 2016a -i TTLL_powheg -n 50 --batchname auto &
+#python python/SKFlat.py -a NonIso -e 2016a -i TTLJ_powheg -n 50 --batchname auto &
+#python python/SKFlat.py -a NonIso -e 2016a -i WW_pythia -n 50 --batchname auto &
+#python python/SKFlat.py -a NonIso -e 2016a -i WWTo2L2Nu_powheg -n 50 --batchname auto &
+#python python/SKFlat.py -a NonIso -e 2016a -i WZ_pythia -n 50 --batchname auto &
+#python python/SKFlat.py -a NonIso -e 2016a -i WZTo3LNu_mllmin4p0_powheg -n 50 --batchname auto &
+
+python python/SKFlat.py -a NonIso -e 2016b -i SingleMuon -n 50 --batchname auto &
+python python/SKFlat.py -a NonIso -e 2016b -i DYJets -n 50 --batchname auto &
+python python/SKFlat.py -a NonIso -e 2016b -i WJets_MG -n 50 --batchname auto &
+python python/SKFlat.py -a NonIso -e 2016b -i TTLL_powheg -n 50 --batchname auto &
+python python/SKFlat.py -a NonIso -e 2016b -i TTLJ_powheg -n 50 --batchname auto &
+python python/SKFlat.py -a NonIso -e 2016b -i WW_pythia -n 50 --batchname auto &
+python python/SKFlat.py -a NonIso -e 2016b -i WWTo2L2Nu_powheg -n 50 --batchname auto &
+python python/SKFlat.py -a NonIso -e 2016b -i WZ_pythia -n 50 --batchname auto &
+python python/SKFlat.py -a NonIso -e 2016b -i WZTo3LNu_mllmin4p0_powheg -n 50 --batchname auto &
